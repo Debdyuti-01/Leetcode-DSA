f7064dcdd3c7a1f4d88e7724a116d8bb2323aec1
Debdyuti Das <48562834+Debdyuti-01@users.noreply.github.com>
2024-04-25 23:43:36 -0700

    Time: 950 ms (54.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+# varchar holds characters of variable length
+# <> means NOT EQUAL TO
+
+SELECT name FROM Customer WHERE referee_id!=2 OR referee_id IS NULL;
+
+
